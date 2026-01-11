7c66e4a0c98590b55e92cb468d3047bb0fb7faa4
Xacce <1524920+xacce@users.noreply.github.com>
2026-01-11 15:50:30 +0300

    c# ver

--- RiftCore/Interpeter/IRiftMem.cs
+++ RiftCore/Interpeter/IRiftMem.cs
@@ -1,14 +1,15 @@
-﻿namespace Rift;
-
-public interface IRiftMem : IDisposable
+﻿namespace Rift
 {
-	public unsafe void WriteBlind<T>(T v) where T : unmanaged;
+	public interface IRiftMem : IDisposable
+	{
+		public unsafe void WriteBlind<T>(T v) where T : unmanaged;
 
-	public unsafe void WriteBlind<T>(T v, int off) where T : unmanaged;
+		public unsafe void WriteBlind<T>(T v, int off) where T : unmanaged;
 
-	public unsafe ref readonly T ReadRoBlit<T>(int offset) where T : unmanaged;
+		public unsafe ref readonly T ReadRoBlit<T>(int offset) where T : unmanaged;
 
-	public unsafe ref T ReadRwBlit<T>(int offset) where T : unmanaged;
+		public unsafe ref T ReadRwBlit<T>(int offset) where T : unmanaged;
 
-	public unsafe byte* ReadRwRaw(int offset);
+		public unsafe byte* ReadRwRaw(int offset);
+	}
 }
